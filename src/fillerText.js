d928c09dcdfa600083e91998f3b3a61dab549458
arielrezinn <arielrezin@gmail.com>
2021-12-10 23:25:58 -0600

    finishing up

---
+++ src/fillerText.js
@@ -0,0 +1,97 @@
+function FillerText() {
+  return (
+    <>
+      <h1>Accessibility in Context</h1>
+
+      The power of the Web is in its universality.
+      Access by everyone regardless of disability is an essential aspect.
+
+      Tim Berners-Lee, W3C Director and inventor of the World Wide Web
+      The Web is fundamentally designed to work for all people, whatever their hardware, software, language, location, or ability. When the Web meets this goal, it is accessible to people with a diverse range of hearing, movement, sight, and cognitive ability.
+
+      Thus the impact of disability is radically changed on the Web because the Web removes barriers to communication and interaction that many people face in the physical world. However, when websites, applications, technologies, or tools are badly designed, they can create barriers that exclude people from using the Web.
+
+      Accessibility is essential for developers and organizations that want to create high-quality websites and web tools, and not exclude people from using their products and services.
+
+      <h1>What is Web Accessibility</h1>
+
+      Web accessibility means that websites, tools, and technologies are designed and developed so that people with disabilities can use them. More specifically, people can:
+
+      perceive, understand, navigate, and interact with the Web
+      contribute to the Web
+      Web accessibility encompasses all disabilities that affect access to the Web.
+
+      Web accessibility also benefits people without disabilities, for example:
+
+      people using mobile phones, smart watches, smart TVs, and other devices with small screens, different input modes, etc.
+      older people with changing abilities due to ageing
+      people with “temporary disabilities” such as a broken arm or lost glasses
+      people with “situational limitations” such as in bright sunlight or in an environment where they cannot listen to audio
+      people using a slow Internet connection, or who have limited or expensive bandwidth
+
+      <h1>More Info on What is Accessibility</h1>
+
+      When you want to learn more about how different disabilities affect Web use, and read about scenarios of people with disabilities using the Web, see How People with Disabilities Use the Web.
+      If you want more examples of benefits for others, see the mobile resource Shared Web Experiences: Barriers Common to Mobile Device Users and People with Disabilities, multimedia Used by People With and Without Disabilities, and the archived Web Accessibility Benefits People With and Without Disabilities.
+      Accessibility is Important for Individuals, Businesses, Society
+
+      The Web is an increasingly important resource in many aspects of life: education, employment, government, commerce, health care, recreation, and more. It is essential that the Web be accessible in order to provide equal access and equal opportunity to people with diverse abilities. Access to information and communications technologies, including the Web, is defined as a basic human right in the United Nations Convention on the Rights of Persons with Disabilities (UN CRPD).
+
+      The Web offers the possibility of unprecedented access to information and interaction for many people with disabilities. That is, the accessibility barriers to print, audio, and visual media can be much more easily overcome through web technologies.
+
+      Accessibility supports social inclusion for people with disabilities as well as others, such as:
+
+      older people
+      people in rural areas
+      people in developing countries
+      There is also a strong business case for accessibility. As shown in the previous section, accessible design improves overall user experience and satisfaction, especially in a variety of situations, across different devices, and for older users. Accessibility can enhance your brand, drive innovation, and extend your market reach.
+
+      Web accessibility is required by law in many situations.
+
+      <h1>More Info on Why Accessibility is Important</h1>
+
+      General information on business benefits is in The Business Case for Digital Accessibility.
+      Examples of the benefits of making audio and video media accessible is in the section Benefits to Organizations.
+      Guidance on figuring out legal requirements is in the archived Legal and Policy Factors.
+      Making the Web Accessible
+
+      Web accessibility depends on several components working together, including web technologies, web browsers and other "user agents", authoring tools, and websites.
+
+      The W3C Web Accessibility Initiative (WAI) develops technical specifications, guidelines, techniques, and supporting resources that describe accessibility solutions. These are considered international standards for web accessibility; for example, WCAG 2.0 is also an ISO standard: ISO/IEC 40500.
+
+      <h1>More Info on Making the Web Accessible</h1>
+
+      More about these aspects of accessibility working together is in Essential Components of Web Accessibility.
+      Web Content Accessibility Guidelines (WCAG), Authoring Tool Accessibility Guidelines (ATAG), ARIA for Accessible Rich Internet Applications, and other important resources are introduced in W3C Accessibility Standards Overview.
+      To learn more about how W3C WAI develops material through multi-stakeholder, international participation and how you can contribute, see About W3C WAI and Participating in WAI.
+      Making Your Website Accessible
+
+      Many aspects of accessibility are fairly easy to understand and implement. Some accessibility solutions are more complex and take more knowledge to implement.
+
+      It is most efficient and effective to incorporate accessibility from the very beginning of projects, so you don’t need go back and to re-do work.
+
+      <h1>More Info on Making Your Website Accessible</h1>
+
+      For an introduction to accessibility requirements and international standards, see Accessibility Principles.
+      To understand some common accessibility barriers from the perspective of testing, see Easy Checks – A First Review of Web Accessibility.
+      For some basic considerations on designing, writing, and developing for accessibility, see Tips for Getting Started.
+      When you’re ready to know more about developing and designing, you’ll probably use resources such as:
+      How to Meet WCAG (Quick Reference)
+      Web Accessibility Tutorials
+      For project management and organizational considerations, see Planning and Managing Web Accessibility.
+      If you need to make quick fixes now, see Web Accessibility First Aid: Approaches for Interim Repairs.
+      Evaluating Accessibility
+
+      When developing or redesigning a website, evaluate accessibility early and throughout the development process to identify accessibility problems early, when it is easier to address them. Simple steps, such as changing settings in a browser, can help you evaluate some aspects of accessibility. Comprehensive evaluation to determine if a website meets all accessibility guidelines takes more effort.
+
+      There are evaluation tools that help with evaluation. However, no tool alone can determine if a site meets accessibility guidelines. Knowledgeable human evaluation is required to determine if a site is accessible.
+
+      <h1>More Info on Evaluating Accessibility</h1>
+
+      Resources to help with accessibility evaluation are described in Evaluating Web Accessibility Overview.
+    </>
+  );
+}
+
+export default FillerText;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FillerText from './fillerText';
+import { Anchor, Box, Button, Grommet, grommet, Heading, Paragraph, Text } from 'grommet';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Grommet theme={grommet}>
+    <div style={{ height: "100vh" }} >
+      <div id='box1' className='quarterbox'>
+        <div style={{ height: "99%", width: "99.5%" }}>
+          <Box overflow={"scroll"} fill={true}>
+            <Heading level={1} size='small' textAlign='center' margin={{ vertical: "small" }}>What is this?</Heading>
+            <Paragraph textAlign='center' size='large' fill={true} margin={{ vertical: "small", horizontal: "medium" }}>
+              This is a website created by Ariel Rezin for use in an assignment for RPSE 100 at the University of Wisconsin-Madison.
+            </Paragraph>
+            <Paragraph textAlign='center' size='large' fill={true} margin={{ vertical: "small", horizontal: "medium" }}>
+              Due to time constraints, the site has not been fully tested for accessibility errors and is not WCAG 2.1 AA compliant.
+              That said, some errors (such as low contrast) are intentional and are used for demonstration purposes.
+            </Paragraph>
+            <Paragraph textAlign='center' size='large' fill={true} margin={{ vertical: "small", horizontal: "medium" }}>
+              Please visit Ariel's <Anchor href="https://arielrezin.com" label="personal website" /> for a11y resourcces or to get in touch with them.
+            </Paragraph>
+          </Box>
+        </div>
+      </div>
+      <div id='box2' className='quarterbox'>
+        <Box
+          width={"50%"}
+          height={"50%"}
+          id='no-cursor'
+          align='center'
+          justify='center'
+        >
+          <Button primary label="submit" margin="xlarge" id='unreachable' size={"large"} />
+        </Box>
+      </div>
+      <div id='box3' className='quarterbox'>
+        <Box width={"35%"}>
+          <Paragraph textAlign='end' size='small'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Review the image to the right for further details.
+          </Paragraph>
+        </Box>
+        <Box
+          width={"35%"}
+          height={"50%"}
+          pad={"small"}
+          margin={"small"}
+          align='end'
+          justify='end'
+          round='medium'
+          background="#d8d8d8"
+          border={{ color: 'brand', size: 'medium' }}>
+          <Text>image 327386.jpg</Text>
+        </Box>
+      </div>
+      <div id='box4' className='quarterbox'>
+        <div style={{ height: "99%", width: "99%" }}>
+          <Box
+            fill={true}
+            overflow={"scroll"}
+            pad={"large"}
+          >
+            <FillerText />
+          </Box>
+        </div>
+      </div>
+    </div >
+  </Grommet>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
